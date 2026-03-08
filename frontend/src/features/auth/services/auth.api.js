0fe27dae4a37dd0f02ad10cc9db03d3852acdeb6
rahulsharma342 <rahulkv247@gmail.com>
2026-03-08 18:53:57 +0530

    version 1.3

--- frontend/src/features/auth/services/auth.api.js
+++ frontend/src/features/auth/services/auth.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/", //backend url
+  baseURL: "https://moodify-faceexpression-song-play.onrender.com/", //backend url
   withCredentials: true, //allow sending cookies with requests
 });
 
